ed7be683d025881fb5ce79a28092a5e36444da5c
ZmbOrk <aleksei05030@gmail.com>
2021-03-16 17:47:25 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Score_VIP = 2_000_000_000;
+        int transfer_money = 500_000_000;
+        int Wallet_VIP = Score_VIP + transfer_money;
+        System.out.println(Wallet_VIP);
+    }
+}
